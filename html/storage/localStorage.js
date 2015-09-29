2e53621e4d50f45d9d248657f6a81a86e818034e
Mauricio Junior <mjuniior92@gmail.com>
2015-09-28 23:43:17 -0300

    localStorage experiments

---
+++ html/storage/localStorage.js
@@ -0,0 +1,24 @@
+(function( win, doc ) {
+  'use strict';
+  var $form = doc.querySelector('[data-js="form"]');
+  var $inputName = doc.querySelector('[data-js="name"]');
+  var $inputEmail = doc.querySelector('[data-js="email"]');
+  var $button = doc.querySelector('[data-js="button"]');
+
+  $button.addEventListener('click', sendMessage, false);
+
+  function sendMessage(event) {
+    localStorage.setItem('name', $inputName.value);
+    localStorage.setItem('email', $inputEmail.value);
+
+    console.log(localStorage);
+
+    console.log('Name', localStorage.getItem('name'));
+    console.log('Email', localStorage.getItem('email'));
+    clearInputs();
+  }
+  function clearInputs() {
+    $inputName.value = '';
+    $inputEmail.value = '';
+  }
+})(window, document);
