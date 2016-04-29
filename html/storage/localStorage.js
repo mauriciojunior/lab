b4b155dfe8c0e27d7533301d66fd54780e53b1e6
Maurício Júnior <mjuniior92@gmail.com>
2016-04-28 23:48:11 -0300

    Update localStorage.js

--- html/storage/localStorage.js
+++ html/storage/localStorage.js
@@ -1,24 +1,25 @@
 (function( win, doc ) {
-  'use strict';
-  var $form = doc.querySelector('[data-js="form"]');
-  var $inputName = doc.querySelector('[data-js="name"]');
-  var $inputEmail = doc.querySelector('[data-js="email"]');
-  var $button = doc.querySelector('[data-js="button"]');
+  'use strict'
+  
+  var $form = doc.querySelector('[data-js="form"]')
+  var $inputName = doc.querySelector('[data-js="name"]')
+  var $inputEmail = doc.querySelector('[data-js="email"]')
+  var $button = doc.querySelector('[data-js="button"]')
 
-  $button.addEventListener('click', sendMessage, false);
+  $button.addEventListener('click', sendMessage, false)
 
   function sendMessage(event) {
-    localStorage.setItem('name', $inputName.value);
-    localStorage.setItem('email', $inputEmail.value);
+    localStorage.setItem('name', $inputName.value)
+    localStorage.setItem('email', $inputEmail.value)
+    
+    console.log(localStorage)
 
-    console.log(localStorage);
-
-    console.log('Name', localStorage.getItem('name'));
-    console.log('Email', localStorage.getItem('email'));
+    console.log('Name', localStorage.getItem('name'))
+    console.log('Email', localStorage.getItem('email'))
     clearInputs();
   }
   function clearInputs() {
-    $inputName.value = '';
-    $inputEmail.value = '';
+    $inputName.value = ''
+    $inputEmail.value = ''
   }
-})(window, document);
+})(window, document)
