a4596a0780e2647a1cddefcb69b7539628a97d9b
Maurício Júnior <mjuniior92@gmail.com>
2016-05-07 21:53:08 -0300

    Update main.js

--- javascript/jquery/examples/main.js
+++ javascript/jquery/examples/main.js
@@ -1,20 +1,12 @@
 (function($) {
-  'use strict';
+  'use strict'
 
   $(document).ready(function() {
     var pictures,
         $menuLinks = $('#menu a'),
         $title = $('head title'),
         $caption = $('.caption'),
-        $img = $('.container img');
-
-    // XML
-    // $.ajax({
-    //   url: 'pictures.xml',
-    //   success: function(data) {
-    //     $pictures = $('picture', data);
-    //   }
-    // });
+        $img = $('.container img')
 
     // JSON
     $.ajax({
@@ -22,15 +14,16 @@
       success: function(data) {
         pictures = data;
       }
-    });
+    })
+    
     $menuLinks.click(function(event) {
-      event.preventDefault();
-      var $index = $menuLinks.index(this);
-      var picture = pictures[$index].picture;
-      $img.attr('src', picture.source);
-      $caption.text(picture.caption);
-      $title.text('Galery 1 - ' + picture.caption);
-    });
-  });
+      event.preventDefault()
+      var $index = $menuLinks.index(this)
+      var picture = pictures[$index].picture
+      $img.attr('src', picture.source)
+      $caption.text(picture.caption)
+      $title.text('Galery 1 - ' + picture.caption)
+    })
+  })
 
-})(jQuery);
+})(jQuery)
